8ecf5933dbc47d0a32af6d73347aeca32eef92b2
srkrishna3384 <srkrishna3384@gmail.com>
2022-08-10 08:33:29 +0530

    Added AugustGitBatch Project

---
+++ src/test/java/com/test/demo/AugustTest.java
@@ -0,0 +1,17 @@
+package com.test.demo;
+
+import org.testng.annotations.Test;
+
+public class AugustTest {
+	@Test
+	public void testOne() {
+		System.out.println("Test One...");
+		System.out.println("***********************");
+	}
+	@Test
+	public void testTwo() {
+		System.out.println("Test Two...");
+		System.out.println("***********************");
+	}
+
+}
